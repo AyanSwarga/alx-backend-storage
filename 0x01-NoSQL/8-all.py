efd984df012991ea3de1a2ec6fa0a327f6ae8a2d
AyanSwarga <ayannicholas@gmail.com>
2023-10-17 15:38:32 +0100

    task_8

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+"""Python function that lists all documents in a collection"""
+
+
+def list_all(mongo_collection):
+    """Function returns all entries in collection"""
+    return mongo_collection.find({})
