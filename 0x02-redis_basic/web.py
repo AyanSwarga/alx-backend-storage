c2760352c4572450ec9303a7c9057128ec1cd01a
AyanSwarga <ayannicholas@gmail.com>
2023-10-20 11:26:25 +0100

    updated

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,28 +1,54 @@
 #!/usr/bin/env python3
-"""Implementing an expiring web cache and tracker"""
+"""In this tasks, we will implement a get_page function
+(prototype: def get_page(url: str) -> str:). The core of
+the function is very simple. It uses the requests module
+to obtain the HTML content of a particular URL and returns it.
+
+Start in a new file named web.py and do not reuse the code
+written in exercise.py.
+
+Inside get_page track how many times a particular URL was
+accessed in the key "count:{url}" and cache the result with
+an expiration time of 10 seconds.
+
+Tip: Use http://slowwly.robertomurray.co.uk to simulate
+a slow response and test your caching."""
+
+
+import redis
 import requests
-from typing import Callable
 from functools import wraps
-_redis = __import__('redis').Redis()
+
+r = redis.Redis()
 
 
-def count(fn: Callable) -> Callable:
-    """Counts number of url calls"""
-    @wraps(fn)
+def url_access_count(method):
+    """decorator for get_page function"""
+    @wraps(method)
     def wrapper(url):
-        _redis.incr(f"count:{url}")
-        res = _redis.get(f"cached:{url}")
-        if res:
-            return res.decode('utf-8')
-        res_val = fn(url)
-        _redis.set(f"count:{url}", 0)
-        _redis.setex(f"cached:{url}", 10, res_val)
-        return res_val
+        """wrapper function"""
+        key = "cached:" + url
+        cached_value = r.get(key)
+        if cached_value:
+            return cached_value.decode("utf-8")
+
+            # Get new content and update cache
+        key_count = "count:" + url
+        html_content = method(url)
+
+        r.incr(key_count)
+        r.set(key, html_content, ex=10)
+        r.expire(key, 10)
+        return html_content
     return wrapper
 
 
-@count
+@url_access_count
 def get_page(url: str) -> str:
-    """Get page on url"""
-    res = requests.get(url)
-    return res.text
+    """obtain the HTML content of a particular"""
+    results = requests.get(url)
+    return results.text
+
+
+if __name__ == "__main__":
+    get_page('http://slowwly.robertomurray.co.uk')
